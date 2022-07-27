6119356a3ba8e8b5b759f2bb48c9678485a225cb
Leonomics <leocv97@gmail.com>
2022-07-27 11:02:48 +0200

    improve carousel

--- js-slider/js/slider.js
+++ js-slider/js/slider.js
@@ -1,4 +1,7 @@
 // console.log('slider')
+const wrapperElement = document.querySelector('.slides-wrapper');
+wrapperElement.innerHTML ='';
+let currentSlideIndex = 0;
 
 const slides = [
 	'./img/01.jpg',
@@ -8,18 +11,24 @@ const slides = [
 	'./img/05.jpg',
 ]
 
-const slideElements = 	[];
+/*const slideElements = 	[];
 
 const slidesWrapperEl = document.querySelector('.slides-wrapper');
 slidesWrapperEl.innerHTML = '';
 console.dir(slidesWrapperEl);
-let currentIndex = 0;
+let currentIndex = 0;*/
 
 for(let i=0; i<slides.length; i++)	{
 	const src = slides[i];
 	//console.log(src);
-	const li = document.createElement('li');
-	li.className = 'slide';
+	//const li = document.createElement('li');
+	const li = `
+	<li class="slide">
+		<img src="${src}">
+	</li>
+	`
+	wrapperElement.innerHTML += li;
+	/*li.className = 'slide';
 	if (i === 0)	{
 		li.classList.add('active');
 	}
@@ -27,10 +36,10 @@ for(let i=0; i<slides.length; i++)	{
 	img.src = src;
 	li.append(img);
 	slidesWrapperEl.append(li);
-	slideElements.push(li);		
+	slideElements.push(li);*/
 }
 
-const nextArrowEl = document.querySelector('.arrow-next');
+/*const nextArrowEl = document.querySelector('.arrow-next');
 console.log(nextArrowEl);
 
 nextArrowEl.addEventListener('click', function ()	{
@@ -44,4 +53,39 @@ nextArrowEl.addEventListener('click', function ()	{
 
 	currentIndex++;
 	//slideSuccessiva.classList.add('active');
+})*/
+
+const slideElements = document.querySelectorAll('.slide');
+slideElements[currentSlideIndex].classList.add('active');
+
+
+
+const nextElement = document.querySelector('.arrow-next');
+const prevElement = document.querySelector('.arrow-prev');
+
+nextElement.addEventListener('click', function(){
+	console.log('go next');
+	const lastIndex = slideElements.length - 1;
+
+	const currentSlide = slideElements[currentSlideIndex];
+	currentSlide.classList.remove('active');
+
+	if(currentSlideIndex < lastIndex)	{
+
+		currentSlideIndex++;
+	}else{
+		currentSlideIndex = 0;
+	}
+	//console.log('slide corrente', currentSlideIndex);
+	
+
+
+	//currentSlideIndex ++;
+	const nextSlide = slideElements[currentSlideIndex];
+	nextSlide.classList.add('active');
+	//console.log('prossima slide', currentSlideIndex, currentSlide);
+})
+
+prevElement.addEventListener('click', function(){
+	console.log('go prev');
 })
